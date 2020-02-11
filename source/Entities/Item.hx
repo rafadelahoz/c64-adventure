8947bdbf349aa211fefb66f79220dfd43676bba0
Rafa de la Hoz <rafa.delahoz@gmail.com>
2020-02-11 20:32:18 +0100

    Keys will be no-color by default, tweaks to piramid

--- source/Entities/Item.hx
+++ source/Entities/Item.hx
@@ -39,16 +39,23 @@ class Item extends Actor
             case "KEY":
                 animation.add("idle", [1]);
                 animation.add("carry", [11]);
-
-                switch (data.properties.flavour) 
+                if (data.properties != null)
                 {
-                    case "NONE":    color = 0xFFbfbfbf;
-                    case "CHERRY":  color = 0xFFf7a8a2;
-                    case "LAPIS":   color = 0xFF87d6dd;
-                    case "ROSE":    color = 0xFFea9ff6;
-                    case "PEAR":    color = 0xFF94e089;
-                    case "LILAC":   color = 0xFFbfb0ff;
-                    case "HONEY":   color = 0xFFbfce72;
+                    switch (data.properties.flavour) 
+                    {
+                        case "NONE":    color = 0xFFbfbfbf;
+                        case "CHERRY":  color = 0xFFf7a8a2;
+                        case "LAPIS":   color = 0xFF87d6dd;
+                        case "ROSE":    color = 0xFFea9ff6;
+                        case "PEAR":    color = 0xFF94e089;
+                        case "LILAC":   color = 0xFFbfb0ff;
+                        case "HONEY":   color = 0xFFbfce72;
+                        default: color = 0xFFbfbfbf; // NONE by default
+                    }
+                } 
+                else 
+                {
+                    color = 0xFFbfbfbf; // NONE by default
                 }
 
             case "APPLE":
